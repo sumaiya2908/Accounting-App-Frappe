ad2cff2d673da37b1cfae5f1999aeb7a8b2baa25
Summayya <frappe@Summayyas-MacBook-Air.local>
2021-07-27 10:33:33 +0530

    create default accounts for new company

--- accounting/accounting/doctype/accounts/accounts.py
+++ accounting/accounting/doctype/accounts/accounts.py
@@ -1,11 +1,14 @@
 # Copyright (c) 2021, summayya and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.utils.nestedset import NestedSet
 
 class Accounts(NestedSet):
 	pass
+
+	def name_with_company(self):
+		self.account_name = self.account_name + "  " + self.company
 
 
 	def before_save(self):
@@ -14,3 +17,5 @@ class Accounts(NestedSet):
 			self.balance_type = 'Debit'
 		else:
 			self.balance_type = 'Credit'
+
+		
--- accounting/accounting/doctype/company/company.py
+++ accounting/accounting/doctype/company/company.py
@@ -1,12 +1,71 @@
 # Copyright (c) 2021, summayya and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-class Company(Document):
-	pass
 
-	def before_save(self):
-		self.abbrevation =  "".join(e[0] for e in self.company_name.split()).capitalize()
-		
+default_accounts = {
+
+    "Application of Funds": ["Debtors",
+                             "Bank Account",
+                             "Cash",
+                             "Stock In Hand"],
+
+
+    "Source of Funds":
+        ["Creditors"],
+
+
+    "Expense":
+        ["Direct Expense"],
+
+
+
+    "Income":
+        ["Income Account"]
+}
+
+root_type = {"Application of Funds": "Assest", "Source of Funds" : "Liability", "Expense" : "Expense", "Income" : "Income" }
+
+class Company(Document):
+
+
+    def create_accounts(self):
+        for account in default_accounts:
+            root_account = frappe.get_doc({
+                "doctype": "Accounts",
+                "account_name": account + " - " + self.abbrevation,
+                "is_group": 1,
+                "root_type": root_type[account],
+                "company": self.company_name
+            })
+            root_account.insert()
+            for child in default_accounts[account]:
+                chile_account = frappe.get_doc({
+                    "doctype": "Accounts",
+                    "account_name": child + " - " + self.abbrevation,
+                    "parent_accounts": root_account.account_name,
+                    "company": self.company_name,
+                    "is_group": 0,
+                    "root_type": root_type[account]
+                })
+                chile_account.insert()
+
+    def before_save(self):
+        abbrevation = "".join(
+            e[0] for e in self.company_name.split()).capitalize()
+        abbr_exist = frappe.db.exists({
+            "doctype" : "Company",
+            "abbrevation" : abbrevation
+        })
+
+        if(abbr_exist):
+            frappe.throw("Abbrevation already used ! ")
+
+        else:
+            self.abbrevation = abbrevation
+
+
+    def on_update(self):
+        self.create_accounts()
